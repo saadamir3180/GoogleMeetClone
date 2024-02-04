21225f76dcb09fb0d6055340764f5ba98f01254f
SAAD AMIR <saadamir318@gmail.com>
2024-02-04 11:50:03 +0500

    refactor: cleanup

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -2,7 +2,7 @@ import { useSearchParams } from "react-router-dom"
 import { useEffect } from 'react';
 
 const Room = () => {
-    const [searchParams, setSearchParams] = useSearchParams()
+    const [searchParams ] = useSearchParams()
     const roomName = searchParams.get('room')
     useEffect(() => {
         // do something with roomName
